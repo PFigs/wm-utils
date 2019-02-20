229f3e42f7440973ee68284e167e99b6bf57b1b1
Joonas Viskari <joonas.viskari@wirepas.com>
2019-02-20 16:36:27 +0200

    lowercasing local vars. Include _final_ hexfiles in flashmenu

--- common/flashmenu.sh
+++ common/flashmenu.sh
@@ -15,35 +15,37 @@ fi
 
 function flash_menu(){
 
-EFR=EFR32MG12P332F1024GL125
-FW_DIR=$1
-#if (( ${#1} == 0 )); then
+    tmp_jlink_file="~/.wmflash.jlink"
+    eval tmp_jlink_file=${tmp_jlink_file}
 
-    cd $FW_DIR
+    EFR=EFR32MG12P332F1024GL125
+    
+    fw_dir=$1
+    old_dir=$PWD
+    cd $fw_dir
 
-    #IMAGES=$(ls *.hex)
-    IMAGES=$(find . -name '*.hex' | cut -c2-)
+    images=$(find . -name '*.hex' | cut -c2- | grep final)
 
-    if [ -z "$IMAGES" ]; then
+    if [ -z "$images" ]; then
 
-        ERR="No images found in $FW_DIR!"
-        ui_errorbox "$ERR"
-        echo $ERR
+        err="No images found in $fw_dir!"
+        ui_errorbox "$err"
+        echo $err
         exit 1
     fi     
     
-    cd $DIR
+    cd ${old_dir}
     
-    IMAGES_ARR=($IMAGES)
-    IMAGES_LEN=${#IMAGES_ARR[@]}
-    if (( ${#IMAGES_LEN} == 0 )); then
-        ERR="No images found!"
-        ui_errorbox "$ERR"
-        echo $ERR
+    images_arr=($images)
+    images_len=${#images_arr[@]}
+    if (( ${#images_len} == 0 )); then
+        err="No images found!"
+        ui_errorbox "$err"
+        echo $err
         exit 1
     fi
 
-    for img in $IMAGES; do
+    for img in $images; do
         if [[ $img == "bl_wm_rtos_app.hex" ]]; then
             rl_imgs="$rl_imgs $img $img 1"
         else
@@ -51,52 +53,57 @@ FW_DIR=$1
         fi
     done
 
-    IMAGE=$(whiptail --notags --title "Flash" --radiolist "Select image" \
-        15 80 $IMAGES_LEN \
+    #TODO: move to whip_ui.sh
+    image=$(whiptail --notags --title "Flash" --radiolist "Select image" \
+        15 80 $images_len \
         $rl_imgs \
         3>&1 1>&2 2>&3)
     stat=$?
+
     if (( $stat != 0 )); then
-        ERR="Cancelled"
-        ui_errorbox "$ERR"
-        echo $ERR
+        err="Cancelled"
+        ui_errorbox "$err"
+        echo $err
         exit 1
     fi
-#else
-#    IMAGE=$1
-#fi
 
-    if [ -z "$IMAGE" ]; then
+    if [ -z "$image" ]; then
 
-        ERR="No image selected!"
-        ui_errorbox "$ERR"
-        echo $ERR
+        err="No image selected!"
+        ui_errorbox "$err"
+        echo $err
         exit 1
     fi 
 
-DEVICES=$(get_device_ids "(FLASH)")
-if ((${#DEVICES} == 0)); then
+    devices=$(get_device_ids "(FLASH)")
+    if ((${#devices} == 0)); then
 
-    ERR="No devices connected."
-    ui_errorbox "$ERR"
-    echo $ERR
-    exit 1
-fi
+        err="No devices connected."
+        ui_errorbox "$err"
+        echo $err
+        exit 1
+    fi
 
-# Set device only if it is not already set via env
-if ((${#JLINK_DEVICE} == 0)); then
-    JLINK_DEVICE=$EFR
-fi
-fw_file=$FW_DIR/$IMAGE
-cmd="gsub(\"DEVICE\", \"$JLINK_DEVICE\"); gsub(\"FIRMWARE\", \"$fw_file\");"
-awk "{$cmd; print}" $FLASH_JLINK > /tmp/flash.jlink
+    # Set device only if it is not already set via env
+    if ((${#JLINK_DEVICE} == 0)); then
+        JLINK_DEVICE=$EFR
+    fi
 
-for DEV in $DEVICES; do
-    JLINK_OPT="-SelectEmuBySN "
-    JLINK_OPT+=${DEV//\"/}
-    JLinkExe $JLINK_OPT -CommanderScript /tmp/flash.jlink
-done
+    fw_file=$fw_dir/${image}
+    echo "selected:"${fw_file}
 
-rm /tmp/flash.jlink
+    cmd="gsub(\"DEVICE\", \"$JLINK_DEVICE\"); gsub(\"FIRMWARE\", \"$fw_file\");"
+    
+    awk "{${cmd}; print}" $FLASH_JLINK > ${tmp_jlink_file}
+
+    for dev in ${devices}; do
+        JLINK_OPT="-SelectEmuBySN "
+        JLINK_OPT+=${dev//\"/}
+        JLinkExe ${JLINK_OPT} -CommanderScript ${tmp_jlink_file}
+    done
+
+    rm ${tmp_jlink_file}
+
+exit 0
 
 }
--- common/jlink.sh
+++ common/jlink.sh
@@ -1,20 +1,31 @@
 #!/usr/bin/env bash
 
+
 if [[ "$BASH_SOURCE" == "$0" ]]
 then
     . "devmenu.sh"
 fi
 
 
-JLINKEXE='JLinkExe'
-if (( ${#EFR} == 0 )); then
-    EFR=EFR32MG12P332F1024GL125
+function connect_device()
+{
+    JLINKEXE='JLinkExe'
+    if (( ${#EFR} == 0 )); then
+        EFR=EFR32MG12P332F1024GL125
+    fi
+
+    devs=$(get_device_ids "JLink")
+    if (( ${#devs} == 0 )); then
+        echo No devices
+        exit 1
+    fi
+    devs_arr=( $devs )
+
+    $JLINKEXE -SelectEmuBySN ${devs_arr[0]} -if swd -device $EFR -autoconnect 1 -speed 4000
+}
+
+if [[ "$BASH_SOURCE" == "$0" ]]
+then
+    connect_device
 fi
 
-devs=$(get_device_ids "JLink")
-if (( ${#devs} == 0 )); then
-    echo No devices
-    exit 1
-fi
-devs_arr=( $devs )
-$JLINKEXE -SelectEmuBySN ${devs_arr[0]} -if swd -device $EFR -autoconnect 1 -speed 4000
